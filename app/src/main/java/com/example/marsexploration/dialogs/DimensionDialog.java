c474dafe4e9f72b585035f5ee64fee5694b04a56
fernandesleticia <leticiafernandes777@gmail.com>
2019-12-07 23:14:29 -0300

    Adição de Dialog para adicionar dimensão de planalto

---
+++ app/src/main/java/com/example/marsexploration/dialogs/DimensionDialog.java
@@ -0,0 +1,28 @@
+package com.example.marsexploration.dialogs;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.marsexploration.R;
+
+public class DimensionDialog extends DialogFragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_dimension, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
--- app/src/main/java/com/example/marsexploration/fragments/MarsFragment.java
+++ app/src/main/java/com/example/marsexploration/fragments/MarsFragment.java
@@ -1,4 +1,40 @@
 package com.example.marsexploration.fragments;
 
-public class MarsFragment extends BaseFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.marsexploration.R;
+import com.example.marsexploration.dialogs.DimensionDialog;
+
+public class MarsFragment extends BaseFragment implements View.OnClickListener {
+
+    Button buttonDimensaoPlanalto;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.mars_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        buttonDimensaoPlanalto = view.findViewById(R.id.button_add_planalto);
+        buttonDimensaoPlanalto.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonDimensaoPlanalto) {
+            DimensionDialog dialogFragment = new DimensionDialog();
+            dialogFragment.show(getActivity().getSupportFragmentManager().beginTransaction(), "dialog_dimension");
+        }
+    }
 }
